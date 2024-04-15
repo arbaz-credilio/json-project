33b339133173a55a7411f7f5c8430153fd848bce
md arbaz  alam <md.alam.27.2@mountblue.tech>
2024-04-15 15:05:25 +0530

    changes in json and id file

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -14,11 +14,9 @@
       >Delete User</UButton
     >
   </UContainer>
-  <NuxtSnackbar />
 </template>
 
 <script setup lang="ts">
-const snackbar = useSnackbar();
 let users = getAllUsers();
 const route = useRoute();
 const id: string | string[] = route.params.id;
@@ -26,19 +24,10 @@ const user = users.value.find((each: any) => each.id.toString() === id);
 
 const deleteAndNavigate = (id: string | string[]) => {
   deleteUser(id);
-  snackbar.add({
-    type: "success",
-    message: `user ${id} is deleted`,
-  });
+ 
   users.value = users.value.filter((each: any) => {
     return each?.id.toString() !== id;
   });
-  setTimeout(() => {
-    navigateTo("/");
-  }, 2000);
+  navigateTo("/");
 };
 </script>
-
-function getAllUsers() {
-  throw new Error("Function not implemented.");
-}
